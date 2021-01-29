c5090bf117038f64f443f0f3fd7db9a0ac3813cc
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2021-01-29 13:00:33 -0300

    fix category route title

--- src/router/routes/categories.js
+++ src/router/routes/categories.js
@@ -12,7 +12,7 @@ export default (componentImport) => [
     path: '/category/:category',
     component: componentImport('categories/Meals'),
     meta: {
-      title: 'Home',
+      title: 'Category',
     },
   },
 ]
